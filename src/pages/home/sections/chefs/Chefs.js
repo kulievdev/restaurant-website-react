efcdc8a6a0ccc0e44e06a7071f08c1768a7d4538
Tohir Kuliev <kuliev.tech@gmail.com>
2023-12-04 15:33:35 -0500

    refactor chefs-component using props

--- src/pages/home/sections/chefs/Chefs.js
+++ src/pages/home/sections/chefs/Chefs.js
@@ -3,6 +3,16 @@ import chef1 from "../../../../assets/betran.png";
 import chef2 from "../../../../assets/ferry.png";
 import chef3 from "../../../../assets/iswan.png";
 
+const Chef = (props) => {
+    return (
+        <div className="chefs-section__chef">
+            <img src={props.img} alt="Betran Chef" />
+            <h4>{props.name}</h4>
+            <p>{props.title}</p>
+        </div>
+    );
+};
+
 const Chefs = () => {
     return (
         <section id="chefs-section">
@@ -11,21 +21,9 @@ const Chefs = () => {
                     Our Greatest <span>Chefs</span>
                 </h2>
                 <div className="chefs-section__chefs">
-                    <div className="chefs-section__chef">
-                        <img src={chef1} alt="Betran Chef" />
-                        <h4>Betran Komar</h4>
-                        <p>Head Chef</p>
-                    </div>
-                    <div className="chefs-section__chef">
-                        <img src={chef2} alt="Ferry Chef" />
-                        <h4>Ferry Sauwi</h4>
-                        <p>Chef</p>
-                    </div>
-                    <div className="chefs-section__chef">
-                        <img src={chef3} alt="Iswan Chef" />
-                        <h4>Iswan Dracho</h4>
-                        <p>Chef</p>
-                    </div>
+                    <Chef img={chef1} name="Betran Komar" title="Head Chef" />
+                    <Chef img={chef2} name="Ferry Sauwi" title="Chef" />
+                    <Chef img={chef3} name="Iswan Dracho" title="Chef" />
                 </div>
                 <button className="btn btn-lg btn-orange">View All</button>
             </div>
