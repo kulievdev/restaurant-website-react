cb497ea4700fb4885f1ea677a6af45a849979ae7
Tohir Kuliev <kuliev.tech@gmail.com>
2024-03-25 20:47:50 -0400

    Make navbar responsive

--- src/design-system/Cart/Cart.js
+++ src/design-system/Cart/Cart.js
@@ -1,11 +1,18 @@
 import cartIcon from "../../assets/cart.svg";
+import useWindowWidth from "../../custom-hooks/useWindowWidth";
 import "./Cart.css";
 
 const Cart = () => {
+    const windowWidth = useWindowWidth();
+
     return (
-        <div className="cart">
-            <img src={cartIcon} alt="Shopping Cart" />
-        </div>
+        <>
+            {windowWidth < 375 ? null : (
+                <div className="cart">
+                    <img src={cartIcon} alt="Shopping Cart" />
+                </div>
+            )}
+        </>
     );
 };
 
