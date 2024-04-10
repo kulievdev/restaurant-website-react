8ae93f5afeb10cd36d0f16f0492f005cb0481888
Tohir Kuliev <kuliev.tech@gmail.com>
2024-04-10 16:49:52 -0400

    Finish date and time picker

--- src/pages/reservation/Reservation.js
+++ src/pages/reservation/Reservation.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { OrangeSpanText, SectionHeading } from "../components/layout";
 import reservationImg from "../../assets/reservation2.png";
+import { useState } from "react";
 
 const ReservationSection = styled.section``;
 
@@ -61,7 +62,93 @@ const ReservationContent = styled.div`
     }
 `;
 
+const InputsWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 5rem;
+    max-width: 50rem;
+`;
+
+const DatePicker = styled.input`
+    border: none;
+    background-color: rgba(250, 250, 249, 1);
+    border-radius: 1rem;
+    padding: 2rem 3rem;
+    outline-color: #ff8a00;
+    color: #5c4529;
+    font-size: 1.6rem;
+
+    &::placeholder {
+        font-size: 1.5rem;
+        line-height: 3rem;
+        color: #a0978c;
+    }
+
+    @media (min-width: 1920px) {
+        padding: 3rem;
+    }
+`;
+
+const TimePicker = styled.input`
+    border: none;
+    background-color: rgba(250, 250, 249, 1);
+    border-radius: 1rem;
+    padding: 2rem 3rem;
+    outline-color: #ff8a00;
+    color: #5c4529;
+    font-size: 1.6rem;
+
+    &::placeholder {
+        font-size: 1.5rem;
+        line-height: 3rem;
+        color: #a0978c;
+    }
+
+    @media (min-width: 1920px) {
+        padding: 3rem;
+    }
+`;
+
+const Select = styled.select`
+    border: none;
+    background-color: rgba(250, 250, 249, 1);
+    border-radius: 1rem;
+    padding: 2rem 3rem;
+    outline-color: #ff8a00;
+    font-size: 1.6rem;
+    color: #5c4529;
+
+    @media (min-width: 1920px) {
+        padding: 3rem;
+    }
+`;
+
+const Option = styled.option`
+    &:first-child {
+        color: #a0978c;
+    }
+`;
+
 const Reservation = () => {
+    const [dateInputType, setDateInputType] = useState("text");
+    const [timeInputType, setTimeInputType] = useState("text");
+
+    const handleDateFocus = () => {
+        setDateInputType("date");
+    };
+
+    const handleDateBlur = () => {
+        setDateInputType("text");
+    };
+
+    const handleTimeFocus = () => {
+        setTimeInputType("time");
+    };
+
+    const handleTimeBlur = () => {
+        setTimeInputType("text");
+    };
+
     return (
         <ReservationSection>
             <ReservationSectionContainer>
@@ -75,6 +162,37 @@ const Reservation = () => {
                     <SectionHeading $marginBottom="5">
                         Book a <OrangeSpanText>table</OrangeSpanText>
                     </SectionHeading>
+                    <InputsWrapper>
+                        <DatePicker
+                            type={dateInputType}
+                            placeholder="Date"
+                            onFocus={handleDateFocus}
+                            onBlur={handleDateBlur}
+                        />
+                        <TimePicker
+                            type={timeInputType}
+                            placeholder="Time"
+                            onFocus={handleTimeFocus}
+                            onBlur={handleTimeBlur}
+                        />
+                        <Select>
+                            <Option>Party size</Option>
+                            <Option>2 People</Option>
+                            <Option>3 People</Option>
+                            <Option>4 People </Option>
+                            <Option>5 People</Option>
+                            <Option>6 People</Option>
+                            <Option>7 People</Option>
+                            <Option>8 People</Option>
+                            <Option>9 People</Option>
+                            <Option>10 People</Option>
+                            <Option>11 People</Option>
+                            <Option>12 People</Option>
+                            <Option>13 People</Option>
+                            <Option>14 People</Option>
+                            <Option>15 People</Option>
+                        </Select>
+                    </InputsWrapper>
                 </ReservationContent>
             </ReservationSectionContainer>
         </ReservationSection>
