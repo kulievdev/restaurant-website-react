2ba07281f35b1cdc55cde8279785661f5af1ccf8
Tohir Kuliev <kuliev.tech@gmail.com>
2024-05-03 15:23:18 -0400

    Remove note

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import Footer from "./pages/landing-page/footer/Footer";
-import Note from "./pages/landing-page/note/Note";
 import TopNavigation from "./pages/landing-page/top-navigation/TopNavigation";
 import { Outlet } from "react-router-dom";
 
 const App = () => {
     return (
         <>
-            <Note />
             <TopNavigation />
             <Outlet />
             <Footer />
